81f38b494a8359e2fae758f45d192909c1f9def3
lamineniang99 <pisupniang@gmail.com>
2024-09-16 22:29:08 +0200

    first commit

---
+++ src/test/java/com/samanecorp/api_springboot/ApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samanecorp.api_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
